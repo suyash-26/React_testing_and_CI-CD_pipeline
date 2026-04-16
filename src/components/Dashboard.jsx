ac8d01eaae64052c1bfaac965659973f6399a585
suyash jaiswal <suyash.jaiswal@antrepriz.com>
2026-04-16 13:07:17 +0530

    changing in main branch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../contexts/AuthContext";
 
 export default function Dashboard() {
   const { isAuthenticated, user } = useAuth();
-
+  // added comments in main branch
   if (!isAuthenticated) return <p>Please login</p>;
 
   return <p>Welcome {user.name}</p>;
